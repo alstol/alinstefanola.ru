e311062a8dac70c4eba8b52cc4663c07d8eb6d99
Alin Stefan Olaru <alin.stefan.olaru@LEGO.com>
2018-09-25 13:00:40 +0200

    Attempting to pre-load images in order to increase render speed

---
+++ files/js/imagepreload.js
@@ -0,0 +1,23 @@
+class ImagePreloader{ 
+    constructor() {
+        this.paths = [];
+        this.init();
+    }
+    collectUrls() {
+        for(var i = 0; i < experienceData.length; i++) 
+            this.paths.push(`${experienceData[i].companyLogo}`)       
+    }
+
+    preloadImages(arrayOfPaths) {
+        $(arrayOfPaths).each(function(){
+            $('<img />').attr('src', this);
+        });
+    }
+    
+    init() {
+        this.collectUrls();
+        this.preloadImages(this.paths);
+    }
+}
+
+var imgpreloader = new ImagePreloader();
