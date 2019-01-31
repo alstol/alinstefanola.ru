1d7501e5862e65540ad43efb44f90e33ee457801
Alin Stefan Olaru <alinstefanolaru@gmail.com>
2019-01-31 21:20:23 +0100

    Added npm, updated readme, added gulp scripts to copy dependencies so they can be accessed

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp')
+
+const basePath = './node_modules/'
+
+const Types = {
+  js: './dist/js',
+  css: './dist/css',
+  font: './dist/fonts',
+}
+
+const Packages = {
+  js: {
+    jquery: `jquery/dist/jquery.js`,
+    vue: `vue/dist/vue.js`,
+  },
+}
+
+gulp.task('default', function(done) {
+  for (let key in Packages) {
+    for (let package in Packages[key]) {
+      let from = `${basePath}${Packages[key][package]}`
+      let to = Types[key]
+      console.log(`Copying "${from}" to "${to}"`)
+      gulp.src(from).pipe(gulp.dest(to))
+    }
+  }
+  done()
+})
