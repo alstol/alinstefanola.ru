3e342dc3db1003383e3b3c99ae7ac635e8192cd4
Alin Stefan Olaru <alinstefanolaru@gmail.com>
2019-02-11 17:41:10 +0100

    Replaced asp.net app w/ regular node app

--- alinstefanola.ru/wwwroot/js/model/bardataset.js
+++
@@ -1,7 +0,0 @@
-class BarDataSet {
-    constructor(name, value) {
-        this.name = name;
-        this.value = value;
-        this.style = `width: ${this.value}%`;
-    }
-}
--- alinstefanola.ru/wwwroot/js/toggle.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(() => {
-  $('.toggle').click(e => {
-    $(
-      `.toggle-area[data-toggleid="${e.currentTarget.dataset.toggleid}"]`
-    ).toggle()
-  })
-})
